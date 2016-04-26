566839aaa03263604dc307a3f211666251822973
Samuel <samuel.linehan15@gmail.com>
2016-04-26 13:59:35 -0500

    working on data request

--- back-end/__init__.py
+++ back-end/__init__.py
@@ -1,6 +1,8 @@
 import os
 from os.path import join, dirname
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+import requests
+import json
 from flask.ext.cors import CORS
 from dotenv import load_dotenv
 
@@ -12,6 +14,22 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hi Sam!"
+
+@app.route("/candidates")
+def get_candidates():
+    key = os.environ.get("CAMPAIGN_FINANCE_API_KEY")
+    headers = {
+        'X-API-Key': key
+    }
+    data = requests.get(CANDIDATE_URL, headers=headers)
+    print (data.json())
+    return data.json
+
+
+@app.route("/test")
+def test():
+    return "This is a test"
+
 
 if __name__ == "__main__":
     app.run()

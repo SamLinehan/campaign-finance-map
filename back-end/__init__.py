f41c7251e28cdb6c8e196085fea468bba9252d7a
Samuel <samuel.linehan15@gmail.com>
2016-06-02 11:27:05 -0400

    added api call timer

--- back-end/__init__.py
+++ back-end/__init__.py
@@ -3,6 +3,7 @@ from os.path import join, dirname
 from flask import Flask, jsonify
 import requests
 import json
+import threading
 from flask.ext.cors import CORS
 from dotenv import load_dotenv
 
@@ -13,20 +14,30 @@ app = Flask(__name__)
 
 CORS(app, resources={r'/*' : {"origins": "*"}}, allow_headers='Content-Type')
 
+
+candidate_data = {}
+
+def get_data():
+    threading.Timer(30.0, get_data).start()
+    key = os.environ.get("CAMPAIGN_FINANCE_API_KEY")
+    url = os.environ.get("CANDIDATE_URL")
+    headers = {
+        'X-API-Key': key
+    }
+    global candidate_data
+    candidate_data = requests.get(url, headers=headers)
+    return
+
+get_data()
+
 @app.route("/")
 def hello():
     return "Welcome to the Campaign Finance Map"
 
 @app.route("/candidates")
 def get_candidates():
-    key = os.environ.get("CAMPAIGN_FINANCE_API_KEY")
-    url = os.environ.get("CANDIDATE_URL")
-    headers = {
-        'X-API-Key': key
-    }
-    data = requests.get(url, headers=headers)
-    print (data.json())
-    return jsonify(data.json())
+    print (candidate_data.json())
+    return jsonify(candidate_data.json())
 
 
 if __name__ == "__main__":
